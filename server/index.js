5861f1721b6dde02906b9ea2cee3d04be49c81d4
Sierra Wendt <serigrace@gmail.com>
2021-08-12 22:53:19 -0700

    installed eslint and fixed a bunch of styling errors, jasmine as well to start implementing unit tests

--- server/index.js
+++ server/index.js
@@ -1,40 +1,37 @@
+/* eslint-disable no-console */
 const express = require('express');
-const db = require('./controllers/index.js')
-const model = require('./models/queries.js')
 const path = require('path');
+// const db = require('./controllers/index');
+const model = require('./models/queries');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-//routes
+// routes
 app.get('/products', (req, res) => {
   model.products
-  .then(product => res.status(200).send(product))
-  .catch(err => console.log('FAILED'))
-
-})
-
+    .then((product) => res.status(200).send(product))
+    .catch((err) => console.log('FAILED', err));
+});
 
 app.get('/products/:product_id', (req, res) => {
   model.product(req.params.product_id)
-  .then(product => res.status(200).send(product))
-  .catch(err => res.status(500).send(err))
-})
-
+    .then((product) => res.status(200).send(product))
+    .catch((err) => res.status(500).send(err));
+});
 
 app.get('/products/:product_id/styles', (req, res) => {
   model.styles(req.params.product_id)
-  .then(style => res.status(200).send(style))
-  .catch(err => res.status(400).send(err))
-})
-
+    .then((style) => res.status(200).send(style))
+    .catch((err) => res.status(400).send(err));
+});
 
 app.get('/products/:product_id/related', (req, res) => {
   model.related(req.params.product_id)
-  .then(related => res.status(200).send(related))
-  .catch(err => res.status(500).send(err))
-})
+    .then((related) => res.status(200).send(related))
+    .catch((err) => res.status(500).send(err));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server listening on port:', PORT));
--- server/models/queries.js
+++ server/models/queries.js
@@ -1,42 +1,37 @@
-const client = require('./index.js');
+/* eslint-disable no-console */
+/* eslint-disable no-else-return */
+const client = require('./index');
 
 module.exports = {
   products:
-    client.query(`select * from products limit 5 offset 600`)
-      .then(res => res.rows )
+    client.query('select * from products limit 5 offset 600')
+      .then((res) => res.rows)
       .catch((err) => console.log('Error in productsDB', err)),
 
-  product: (id = 999) => {
-    return client
-      .query(`select * from products where id = ${id}`)
-      .then(res => res.rows )
-      .catch((err) => console.log('Error in productDB', err))
-  },
+  product: (id) => client
+    .query(`select * from products where id = ${id}`)
+    .then((res) => res.rows)
+    .catch((err) => console.log('Error in productDB', err)),
 
-  styles: (id) => {
-    return client
-      .query(`select * from styles where product_id = ${id}`)
-      .then((res) =>  {
-        if (res.rows.length) {
-          return res.rows;
-        } else {
-          return 'No styles for this item';
-        }
-      })
-      .catch((err) => console.log('Error in stylesDB', err.stack))
-  },
+  styles: (id) => client
+    .query(`select * from styles where product_id = ${id}`)
+    .then((res) => {
+      if (res.rows.length) {
+        return res.rows;
+      } else {
+        return 'No styles for this item';
+      }
+    })
+    .catch((err) => console.log('Error in stylesDB', err.stack)),
 
-  related: (id) => {
-    return client
-      .query(`select related_id from related_products where product_id = ${id}`)
-      .then((res) =>  {
-        if (res.rows.length) {
-          return res.rows;
-        } else {
-          return 'No related products for this item';
-        }
-      })
-      .catch(err => console.log('Error in relatedDB', err))
-  }
-
-}
+  related: (id) => client
+    .query(`select related_id from related_products where product_id = ${id}`)
+    .then((res) => {
+      if (res.rows.length) {
+        return res.rows;
+      } else {
+        return 'No related products for this item';
+      }
+    })
+    .catch((err) => console.log('Error in relatedDB', err)),
+};
