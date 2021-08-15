d8e21e843d34d7336b4c2d1b2370e6b8aac86c7c
Sierra Wendt <serigrace@gmail.com>
2021-08-15 16:50:52 -0700

    completed the query and API for product details

--- db/models/helperQuery.js
+++ db/models/helperQuery.js
@@ -1,5 +1,15 @@
 const client = require('./index');
 
+const test = `SELECT *,
+(SELECT json_agg(row_to_json(features))
+FROM (
+  SELECT feature, value from features
+  WHERE product_id = p.id)
+features)
+AS features
+FROM products AS p
+WHERE p.id = 6`;
+
 module.exports = {
   getProducts: () => {
     const pQuery = 'SELECT * FROM products LIMIT 5 OFFSET 500';
@@ -8,7 +18,16 @@ module.exports = {
       .catch((err) => console.log('Error in getProducts', err));
   },
   getProductDetails: (id) => {
-    const pdQuery = 'SELECT * FROM products WHERE id = $1';
+    const pdQuery = `SELECT *,
+    (SELECT json_agg(row_to_json(features))
+    FROM (
+      SELECT feature, value from features
+      WHERE product_id = p.id)
+    features)
+    AS features
+    FROM products AS p
+    WHERE p.id = $1`;
+
     let value = [`${id}`];
     return client.query(pdQuery, value)
       .then((res) => res.rows)
